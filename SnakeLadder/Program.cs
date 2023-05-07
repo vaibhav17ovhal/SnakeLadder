f03264bb2a472556b303b7c96467f6599bbaef85
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-07 14:35:40 +0530

    [Vaibhav] Add:Position of player is at 0

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+        }
+    }
+}
