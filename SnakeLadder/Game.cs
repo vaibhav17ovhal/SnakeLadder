70d5b85482ea296b90328cce1b3d650d9922685f
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-07 14:55:13 +0530

    [Vaibhav] Add:Player rolls a die to get a number

--- SnakeLadder/Game.cs
+++ SnakeLadder/Game.cs
@@ -9,5 +9,16 @@ namespace SnakeLadder
     public class Game
     {
         int playerPosition = 0;
+        int count = 0;
+
+        Random random = new Random();
+
+        public int DieRoll()
+        {
+            int num = random.Next(1,7);
+            Console.WriteLine($"Die Roll: -----> {num}");
+            count++;
+            return num;
+        }
     }
 }
--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -7,6 +7,8 @@ namespace SnakeLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
+            Game game = new Game();
+            game.DieRoll();
         }
     }
 }
